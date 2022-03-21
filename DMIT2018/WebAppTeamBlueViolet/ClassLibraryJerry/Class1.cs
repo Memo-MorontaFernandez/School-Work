2f9b7b8f9ece1288f5c904c35f1ae93b7106fbf5
Guillermo Moronta Fernandez <90333088+Memo-MorontaFernandez@users.noreply.github.com>
2022-03-20 18:13:14 -0600

    Add files via upload

---
+++ DMIT2018/WebAppTeamBlueViolet/ClassLibraryJerry/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace ClassLibraryJerry
+{
+    public class Class1
+    {
+    }
+}
---
+++ DMIT2018/WebAppTeamBlueViolet/WebAppTeamBlueViolet/Pages/Index.cshtml.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAppTeamBlueViolet.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+        private readonly IConfiguration _config;
+            
+        public string DemoPurposesOnly { get; set; }
+        public IndexModel(ILogger<IndexModel> logger, IConfiguration config)
+        {
+            _logger = logger;
+            _config = config;
+        }
+
+        public void OnGet()
+        {
+            DemoPurposesOnly = _config.GetConnectionString("DefaultConnection");
+
+        }
+    }
+}
