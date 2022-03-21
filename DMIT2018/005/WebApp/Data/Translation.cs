de0816e10a1d38c3ddfe6b551060905bbe7f87dd
Guillermo Moronta Fernandez <90333088+Memo-MorontaFernandez@users.noreply.github.com>
2022-03-20 18:25:15 -0600

    Add files via upload

---
+++ DMIT2018/005/WebApp/Data/Translation.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApp.Data
+{
+    public class Translation
+    {
+        public string English { get; set; }
+
+        public string Klingon { get; set; }
+
+    }
+}
